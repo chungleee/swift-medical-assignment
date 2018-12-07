a08e43b15b546254ee1e5a71c597bc327e41de9c
Leon <leon.karho.chung@gmail.com>
2018-12-07 15:07:40 -0500

    change text size

--- src/components/PatientDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PatientDetail = ({ match }) => {
-	return (
-		<div>{ match.params.patientId }</div>
-	)
-}
-
-export default PatientDetail
--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -40,7 +40,7 @@ class PatientList extends Component {
 	render() {
 		return (
 			<div>
-				<h1 className='is-size-3'>Patient List Component</h1>
+				<h1 className='is-size-2'>Patient List Component</h1>
 				<ul>
 					{ this.renderList() }
 				</ul>
