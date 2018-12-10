9257a082701b771991d4fc271584f09c8a2b34d2
Leon <leon.karho.chung@gmail.com>
2018-12-09 23:15:52 -0500

    getPatientInfo and getPatientWounds actions

--- src/actions/patientActions.js
+++ src/actions/patientActions.js
@@ -1,4 +1,4 @@
-import { GET_PATIENTS } from './types'
+import { GET_PATIENTS, GET_PATIENT_INFO, GET_PATIENT_WOUNDS } from './types'
 import axios from 'axios'
 
 export const getPatients = () => {
@@ -15,5 +15,36 @@ export const getPatients = () => {
 			.catch((error) => {
 				console.log(error)
 			})	
+	}
+}
+
+export const getPatientInfo = (id) => {
+	return (dispatch) => {
+		axios
+			.get(`http://0.0.0.0:3000/patients/${id}`)
+			.then((response) => {
+				dispatch({
+					type: GET_PATIENT_INFO,
+					payload: response.data.data.attributes
+				})
+			})
+			.catch((error) => {
+				console.log(error)	
+			})	
+	}
+}
+
+export const getPatientWounds = (id) => {
+	return (dispatch) => {
+		axios.get(`http://0.0.0.0:3000/patients/${id}/wounds`)
+			.then((response) => {
+				dispatch({
+					type: GET_PATIENT_WOUNDS,
+					payload: response.data.data
+				})
+			})
+			.catch((error) => {
+				console.log(error)
+			})
 	}
 }
