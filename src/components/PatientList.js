f893521d7151e7b9e61a2e56f24819fa16198108
Leon <leon.karho.chung@gmail.com>
2018-12-09 23:17:04 -0500

    remove axios

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getPatients } from '../actions/patientActions'
-import axios from 'axios'
 
 class PatientList extends Component {
 	// // get list of patient
